5e411aaad5a91fb00f64abc599eb449f7704efd8
abdullah-zengin <abdullah.zengin@anywai.ch>
2024-10-26 19:10:35 +0300

    feat(backend): add cors, class validator, and morgan logging

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,15 +2,16 @@ import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MorganMiddleware } from '@nest-middlewares/morgan';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [ConfigModule.forRoot({ isGlobal: true })],
+    controllers: [AppController],
+    providers: [AppService],
 })
 export class AppModule implements NestModule {
-  configure(consumer: MiddlewareConsumer) {
-    MorganMiddleware.configure('common');
-    consumer.apply(MorganMiddleware).forRoutes('*');
-  }
+    configure(consumer: MiddlewareConsumer) {
+        MorganMiddleware.configure('common');
+        consumer.apply(MorganMiddleware).forRoutes('*');
+    }
 }
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+    const app = await NestFactory.create(AppModule);
+
+    app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+
+    const configService: ConfigService = app.get(ConfigService);
+
+    app.enableCors();
+
+    await app.init();
+
+    const port: number = configService.get<number>('NODE_SERVER_PORT', 3000);
+
+    await app.listen(port, () => {
+        console.log(`[🚀 ready ] http://localhost:${port}`);
+    });
 }
 bootstrap();

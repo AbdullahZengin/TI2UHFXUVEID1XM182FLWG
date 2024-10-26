abff12178ff7862e008f300f7db627ef8b4eae54
abdullah-zengin <abdullah.zengin@anywai.ch>
2024-10-26 18:58:25 +0300

    feat(backend): add morgan logging middleware

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,16 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MorganMiddleware } from '@nest-middlewares/morgan';
 
 @Module({
   imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    MorganMiddleware.configure('common');
+    consumer.apply(MorganMiddleware).forRoutes('*');
+  }
+}
